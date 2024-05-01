8815327dd494d5de232f6d356845eb4e347a4a06
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-05-02 00:04:20 +0530

    Dev: Basic layout for the learn page with header, feed-wrapper, sticky-wrapper and user-progress components

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from "./ui/button";
+import { InfinityIcon } from "lucide-react";
 
 type Props = {
   activeCourse: { imageSrc: string; title: string }; // TODO: Replace with DB types
@@ -31,7 +32,30 @@ export const UserProgress = ({
       </Link>
       <Link href={"/shop"}>
         <Button variant={"ghost"} className="text-orange-500">
-          <Image width={28} height={28} src={"/points.svg"} alt="Points" />
+          <Image
+            width={28}
+            height={28}
+            src={"/points.svg"}
+            alt="Points"
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href={"/shop"}>
+        <Button variant={"ghost"} className="text-rose-500">
+          <Image
+            width={28}
+            height={28}
+            src={"/heart.svg"}
+            alt="Hearts"
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
         </Button>
       </Link>
     </div>
