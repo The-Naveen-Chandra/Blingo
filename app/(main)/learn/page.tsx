2b5360d927a940036c6abd816327919dc7bab28e
Naveen Chandra <115704630+The-Naveen-Chandra@users.noreply.github.com>
2024-05-08 22:11:04 +0530

    Dev: getUnits query for getting the units and Modify the seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 
 import { Header } from "./header";
 import { FeedWrapper } from "@/components/feed-wrapper";
@@ -8,9 +8,13 @@ import { UserProgress } from "@/components/user-progress";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 
 const LearnPage = async () => {
+  const unitsData = getUnits();
   const userProgressData = getUserProgress();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([
+    userProgressData,
+    unitsData,
+  ]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
@@ -29,6 +33,11 @@ const LearnPage = async () => {
 
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -14,6 +14,11 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
 
     await db.insert(schema.courses).values([
       {
@@ -45,6 +50,138 @@ const main = async () => {
         id: 6,
         title: "Italian",
         imageSrc: "/it.svg",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1, // Hindi
+        title: "Unit 1",
+        description: "Learn the basics of Hindi",
+        order: 1,
+      },
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1, // Unit 1 (Learn the basic of Hindi)
+        order: 1,
+        title: "Nouns",
+      },
+      // {
+      //   id: 2,
+      //   unitId: 1, // Unit 1 (Learn the basic of Hindi)
+      //   order: 2,
+      //   title: "Verbs",
+      // },
+      // {
+      //   id: 3,
+      //   unitId: 1, // Unit 1 (Learn the basic of Hindi)
+      //   order: 3,
+      //   title: "Adjectives",
+      // },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonsId: 1, // Nouns
+        type: "SELECT",
+        order: 1,
+        question: 'What is the Hindi word for "apple"?',
+      },
+      // {
+      //   id: 2,
+      //   lessonsId: 1, // Nouns
+      //   type: "SELECT",
+      //   order: 2,
+      //   question: 'What is the Hindi word for "banana"?',
+      // },
+      // {
+      //   id: 3,
+      //   lessonsId: 1, // Nouns
+      //   type: "SELECT",
+      //   order: 3,
+      //   question: 'What is the Hindi word for "cat"?',
+      // },
+      // {
+      //   id: 4,
+      //   lessonsId: 2, // Verbs
+      //   type: "SELECT",
+      //   order: 1,
+      //   question: 'What is the Hindi word for "run"?',
+      // },
+      // {
+      //   id: 5,
+      //   lessonsId: 2, // Verbs
+      //   type: "SELECT",
+      //   order: 2,
+      //   question: 'What is the Hindi word for "jump"?',
+      // },
+      // {
+      //   id: 6,
+      //   lessonsId: 2, // Verbs
+      //   type: "SELECT",
+      //   order: 3,
+      //   question: 'What is the Hindi word for "eat"?',
+      // },
+      // {
+      //   id: 7,
+      //   lessonsId: 3, // Adjectives
+      //   type: "SELECT",
+      //   order: 1,
+      //   question: 'What is the Hindi word for "big"?',
+      // },
+      // {
+      //   id: 8,
+      //   lessonsId: 3, // Adjectives
+      //   type: "SELECT",
+      //   order: 2,
+      //   question: 'What is the Hindi word for "small"?',
+      // },
+      // {
+      //   id: 9,
+      //   lessonsId: 3, // Adjectives
+      //   type: "SELECT",
+      //   order: 3,
+      //   question: 'What is the Hindi word for "fast"?',
+      // },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1, // What is the Hindi word for "apple"?
+        text: "सेब",
+        correct: true,
+        imageSrc: "/apple.svg",
+        audioSrc: "/in_apple.mp3",
+      },
+      {
+        id: 2,
+        challengeId: 1, // What is the Hindi word for "apple"?
+        text: "केला",
+        correct: false,
+        imageSrc: "/banana.svg",
+        audioSrc: "/in_banana.mp3",
+      },
+      {
+        id: 3,
+        challengeId: 1, // What is the Hindi word for "apple"?
+        text: "बिल्ली",
+        correct: false,
+        imageSrc: "/cat.svg",
+        audioSrc: "/in_cat.mp3",
+      },
+      {
+        id: 4,
+        challengeId: 1, // What is the Hindi word for "apple"?
+        text: "गाय",
+        correct: false,
+        imageSrc: "/cow.svg",
+        audioSrc: "/in_cow.mp3",
       },
     ]);
 
